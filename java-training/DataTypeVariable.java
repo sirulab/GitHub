ac2ecf5e634d913579cbe45021dab38b2cbcf726
sirulab <chensiru.sjtu@gmail.com>
2026-06-19 11:59:09 +0800

    [process]

---
+++ java-training/DataTypeVariable.java
@@ -0,0 +1,6 @@
+public class DataTypeVariable{
+    public static void main(String[] args){
+        System.out.println(3);
+        System.out.println(120);
+    }
+}
---
+++ java-training/Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+ public static void main(String[] args){
+    System.out.println("Hello Java");
+ }
+}
+//Hello和檔名一樣
+//類別的定義裡面,建立一個主要的方法
+//terminal: cd path
+//terminal: cd ..
+//terminal: javac Hello.java
+//terminal: java Hello (記得先存檔)(執行)
